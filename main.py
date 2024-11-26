51e1caf8228ebfd09e0c38f0a8360b76a31068f6
danil <rush09-09@mail.ru>
2024-11-26 23:36:00 +0300

    Добавлена возможность отображениякарты города в браузере при нажатии на кнопку

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from future.backports import count
 from opencage.geocoder import OpenCageGeocode
 from tkinter import *
+import webbrowser
 
 def get_coordinates(city,key):
     try:
@@ -10,11 +10,18 @@ def get_coordinates(city,key):
             lat = round(results[0]['geometry']['lat'], 2)
             lon = round(results[0]['geometry']['lng'], 2)
             country = results[0]['components']['country']
+            osn_url = f'https://www.openstreetmap.org/?mlat={lat}&mlon={lon}'
             if  'state' in results[0]['components']:
                 region = results[0]['components']['state']
-                return f'Широта: {lat}, Долгота:{lon} \n Страна: {country},\n регион: {region}'
+                return {
+                    'coordinates': f'Широта: {lat}, Долгота:{lon} \n Страна: {country},\n регион: {region}',
+                    'map_url': osn_url
+                        }
             else:
-                return f'Широта: {lat}, Долгота:{lon} \n Страна: {country}'
+                return {
+                    'coordinates': f'Широта: {lat}, Долгота:{lon} \n Страна: {country}',
+                    'map_url': osn_url
+                }
         else:
             return 'Город не найден'
     except Exception as e:
@@ -22,16 +29,23 @@ def get_coordinates(city,key):
 
 
 def show_coordinates(event=None):
+    global map_url
     city = entry.get()
-    coordinates = get_coordinates(city, key)
-    label.config(text=f'Координаты города {city}:\n {coordinates}')
+    result = get_coordinates(city, key)
+    label.config(text=f'Координаты города {city}:\n {result['coordinates']}')
+    map_url = result['map_url']
 
+
+def show_map():
+    if map_url:
+        webbrowser.open(map_url)
 
 key = '49f2646c23fe46299695b0573c0b5080'
+map_url = ''
 
 window=Tk()
 window.title('Координаты городов')
-window.geometry('320x120')
+window.geometry('320x160')
 
 entry = Entry()
 entry.pack()
@@ -42,5 +56,8 @@ button.pack()
 
 label = Label(text='Введите город и нажмите на кнопку')
 label.pack()
+
+mup_button = Button(text='Показать карту', command=show_map)
+mup_button.pack()
 
 window.mainloop()
